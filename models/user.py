c053a1f61f5328b72365ffcaff2f7abea256895f
Eldad <eldadf456@gmail.com>
2024-04-17 18:20:19 -0700

    AirBnB_clone_v2

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""User class"""
+"""This is the user class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
@@ -9,7 +9,7 @@ from models.review import Review
 
 
 class User(BaseModel, Base):
-    """class user
+    """This is the class for user
     Attributes:
         email: email address
         password: password for you login
